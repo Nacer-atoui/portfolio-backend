c4a30d41b13f823e030f8c1c7894267a53303032
Mon Nom <mon.email@exemple.com>
2026-07-13 13:37:48 +0200

    rectification try/catch

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,42 +1,33 @@
-import AppError from "../errors/AppError.js";
 import * as ProjectService from '../services/project.service.js';
 
-
-export const getAll = async (req, res, next) => {
+export const getAll = async (req, res) => {
   const project = await ProjectService.getAllProject();
   res.json(project);
 };
 
 export const getById = async (req, res) => {
-    const project = await ProjectService.getProjectById(req.params.id);
-    res.json(project);
+  const project = await ProjectService.getProjectById(req.params.id);
+  res.json(project);
 };
 
-
 export const newProject = async (req, res) => {
-    try {
-        const data = req.body;
-        const idUtilisateur = req.user.id; 
-        const projectData = {
-            ...data,
-            users_id: idUtilisateur 
-        };
+  const data = req.body;
+  const idUtilisateur = req.user.id; 
+  const projectData = {
+    ...data,
+    users_id: idUtilisateur 
+  };
 
-        const project = await ProjectService.createProject(projectData);
-        
-        return res.status(201).json(project);
-    } catch (error) {
-        console.error("Erreur lors de la création :", error);
-        return res.status(500).json({ message: "Erreur serveur lors de la création du projet." });
-    }
-}
+  const project = await ProjectService.createProject(projectData);
+  res.status(201).json(project);
+};
 
 export const projectUpdate = async (req, res) => {
-    const { id } = req.params;
-    const data = req.body;
-    const updatedProject = await ProjectService.updateProject(id, data);
-    return res.status(200).json(updatedProject);
-}
+  const { id } = req.params;
+  const data = req.body;
+  const updatedProject = await ProjectService.updateProject(id, data);
+  res.status(200).json(updatedProject);
+};
 
 export const projectDelete = async (req, res) => {
   await ProjectService.deleteProject(req.params.id);
