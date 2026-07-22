da4402e3ebbdf1ad8d9588c7339c2330a75f692e
Mon Nom <mon.email@exemple.com>
2026-07-22 10:10:22 +0200

    mise à jour environnement dev et prod

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,21 @@
 import mysql from 'mysql2/promise';
- 
-const pool = mysql.createPool({
+
+// 1. On prépare la configuration de base
+const dbConfig = {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false },
-});
- 
+  connectionLimit: 10 // En local, on peut être généreux
+};
+
+// 2. On ajoute le SSL uniquement si la variable d'environnement le demande
+if (process.env.DB_SSL === 'true') {
+  dbConfig.ssl = { rejectUnauthorized: false };
+}
+
+// 3. On crée le pool
+const pool = mysql.createPool(dbConfig);
+
 export default pool;
